4e5b36f2b7e846e9e332216919b26e1ac3746963
León Abascal <leonabascal@opendeusto.es>
2021-04-06 02:48:25 +0200

    Agregando comentarios estándar a las funciones v3
    Añadidos comentarios a stdin_fix.h
    + clean_stdin
    + clean_buffer
    + confirm_action
    + press_to_continue
    
    Estándar: https://www.cs.cmu.edu/~410/doc/doxygen.html

--- include/stdin_fix.h
+++ include/stdin_fix.h
@@ -3,7 +3,15 @@
 
 #define DEFAULT_BUFFER_SIZE 10 // Tamaño por defecto para el buffer de caracteres
 
+/**
+ * @brief Limpia el buffer de entrada de la consola (stdin)
+ */
 void clear_stdin();
+
+/**
+ * @brief Si estamos usando un buffer que no es stdin, lo limpia
+ * @param buffer El buffer a limpiar
+ */
 void clean_buffer(char* buffer);
 void scan_str(char* buffer, int buffer_size);
 
@@ -12,10 +20,20 @@ char* read_str(const char* message);
 int read_int(const char* message);
 float read_float(const char* message);
 
-// Confirmación del usuario
+/**
+ * @brief Lanza por consola un mensaje de Sí/No
+ * @param message Mensaje de confirmación a mostrar
+ * @return 0 = no, 1 = sí
+ */
 int confirm_action(const char* message);
 
-// Esperar hasta entrada de usuario
+/**
+ * @brief Espera a un enter del usario para continuar
+ *
+ * Muestra el mensaje "Pulsa intro para continuar..."
+ * Es un putchar, así que al puslar enter/intro escapa.
+ *
+ */
 void press_to_continue();
 
 #endif
