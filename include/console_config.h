e74be620a240e68832ccd616dca313ef577f0361
León Abascal <leonabascal@opendeusto.es>
2021-04-06 02:01:08 +0200

    Agregando comentarios estándar a las funciones
    Añadidos comentarios a console_config.h
    + file
    + setup_console
    + restore_console
    + printf_c
    + clear_screen
    + test_nums_printf_c
    + test_each_printf_c
    
    Estándar: https://www.cs.cmu.edu/~410/doc/doxygen.html

--- include/console_config.h
+++ include/console_config.h
@@ -1,3 +1,12 @@
+/**
+ *  @file console_config.h
+ *  @brief Funciones para el output por consola
+ *
+ *  @author Jon Andoni Castillo
+ *  @author León Abascal
+ *  @bug Desconocidos
+*/
+
 #ifndef CONSOLE_CONFIG_H
 
     #define CONSOLE_CONFIG_H
@@ -34,20 +43,64 @@
 
     // FUNCIONES DE UTILIDAD
 
-    // Prepara la consola para que acepte códigos ANSI de colores
-
+    /**
+     * @brief Prepara la consola para que acepte códigos ANSI de colores
+     *
+     * La llamamos al principio del todo para que la consola sea capaz
+     * de asimilar los colores ANSI y podamos usar la función printf_c
+     * Para revertir este efecto hay que llamar a restore_console al
+     * final del programa.
+     *
+     */
     void setup_console();
 
+    /**
+     * @brief Devuelve la consola a su configuración previa
+     *
+     * La llamamos después de setup_console, al final del programa
+     */
     void restore_console();
 
     // Imprime el texto con un color dado
+    /**
+     * @brief Imprime por consola un mensaje, pero se formatea para que tenga color
+     *
+     * Permite formato como en printf con los argumentos variables.
+     * Existe opción de que color sea subrayado (UNDERLINE) pero hay
+     * que hacer saltos de línea en otro buffer para romperlo.
+     *
+     * @param color Puede ser un color de nuestra enum o puede ser sus valores en ANSI.
+     * @param format Mensaje a mostrar con dicho color.
+     */
     void printf_c(ANSI_COLOR color, const char* format, ...);
 
     // Limpia la pantalla (funciona en Linux y Windows (32 y 64), en otros OS la funcionalidad es nula)
+    /**
+     * @brief Limpia la pantalla
+     *
+     * Funciona para Linux y Windows (32 y 64 bits) como se espera
+     * Para SO desconocidos, tiraremos un putchar('\n')
+     */
     void clear_screen();
 
+
     // Imprime por pantalla todos los colores de consola posibles
-    void test_nums_printf_c(); // con sus respectivos números
-    void test_each_printf_c(); // con sus respectivos nombres
+    /**
+     * @brief Imprime por consola los colores disponibles
+     *        con sus respectivos números ANSI.
+     *
+     * Los colores son los estándar de ASNI para nuestro printf_c.
+     * Este método es de muestra.
+     */
+    void test_nums_printf_c();
+
+    /**
+     * @brief Imprime por consola los colores disponibles
+     *        con sus respectivos nombres
+     *
+     * Los colores son los estándar de ASNI para nuestro printf_c.
+     * Este método es de muestra.
+     */
+    void test_each_printf_c();
 
 #endif // CONSOLE_CONFIG_H
