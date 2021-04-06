bb5d5c2c69591b6070f1fdeadb93df18c41167f4
León Abascal <leonabascal@opendeusto.es>
2021-04-06 02:45:57 +0200

    Agregando comentarios estándar a las funciones v2
    Añadidos comentarios a management.h
    + register_profit
    + register_expense
    + check_profit
    + check_expenses
    
    Estándar: https://www.cs.cmu.edu/~410/doc/doxygen.html

--- include/management.h
+++ include/management.h
@@ -1,11 +1,40 @@
 #ifndef MANAGEMENT_H
 #define MANAGEMENT_H
 
-
+/**
+ * @brief Dado un beneficio, lo registra en el archivo de balance
+ *
+ * Pensado para usarse cuando se realize una operación que nos
+ * dé dinero.
+ *
+ * @param profit Beneficio de la operación
+ */
 void register_profit(float profit);
+
+/**
+ * @brief Dado un gasto, lo registra en el archivo de balance
+ *
+ * Pensado para usarse cuando se realize una operación en la
+ * que gastemos dinero.
+ *
+ * @param expense Gasto realizado en la operación
+ */
 void register_expense(float expense);
 
+
+/**
+ * @brief Nos da el beneficio total (solo positivos)
+ *
+ * No confundirse con el beneficio neto (beneficio - gasto).
+ *
+ * @return Regresa beneficio total que hemos acumulado
+ */
 float check_profit();
+
+/**
+ * @brief Nos da el gasto total (solo negativo)
+ * @return Regresa gasto total que hemos acumulado
+ */
 float check_expenses();
 
 #endif
